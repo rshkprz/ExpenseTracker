ceb5b2a1fcc31cfb382f1ecb1c4e4d90ce5c215b
Flawyer <Nimadongtamang1@gmail.com>
2024-04-30 22:19:47 +0545

    New Feature Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import './App.css'
-import Card from './components/card'
-import Header from './components/header'
-import MenuBar from './components/menubar'
+import './App.css';
+import Card from './components/card';
+import Header from './components/header';
+import MenuBar from './components/menubar';
+import { IncomeExpenseProvider } from './context/ApiContext';
 
 function App() {
   return (
-    <div className='h-screen'>
-      <Header />
-      
+    <IncomeExpenseProvider>
+      <div className='h-screen'>
+        <Header />
+        
         <div className='card flex flex-col sm:flex-row items-center justify-center gap-5'>
-          <Card title="Income"/>
-          <Card title="Expense"/>
+          <Card title="Income" />
+          <Card title="Expense" />
         </div>
-     
+       
         <div className='menu-container mt-4'>
           <MenuBar />
         </div>
-    </div>
-  )
+      </div>
+    </IncomeExpenseProvider>
+  );
 }
 
-export default App
+export default App;
